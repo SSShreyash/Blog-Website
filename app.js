495470e723caac73844514cc903203d127125e80
shrey <shreyash4526@gmail.com>
2023-09-16 19:00:20 +0530

    optimize

--- app.js
+++ app.js
@@ -4,20 +4,14 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
 const mongoose = require("mongoose");
-
 mongoose.connect(process.env.URI);
 
-const blogSchema = new mongoose.Schema({
-    head: String,
-    body: String
-});
+const blogSchema = new mongoose.Schema({head: String, body: String});
 
 const Post = mongoose.model("Post", blogSchema);
 
 const app = express();
-
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
@@ -26,61 +20,41 @@ const aboutContent = "This is a blog website where you may share your thoughts w
 const contactContent = "Made by Shreyash Patil.";
 
 app.get("/", function (req, res){
-
     Post.find().then(function (posts){
-        res.render("home", {StartingContent : homeStartingContent,
-                            posts : posts});
-    }).catch(function (err){
-        console.log(err);
-    });
-    
+        res.render("home", {StartingContent:homeStartingContent, posts:posts});
+    }).catch(function(err){console.log(err)});
 });
 
 app.get("/about", function(req, res){
-
   res.render("about", {StartingContent: aboutContent});
 });
 
 app.get("/contact", function(req, res){
-
   res.render("contact", {StartingContent: contactContent});
 });
 
 app.get("/compose", function(req, res){
-
   res.render("compose", {});
 });
 
 app.post("/compose", function(req, res){
+    const newPost = {head:req.body.newContentHead, body:req.body.newContent}
 
-  const newPost = {
-      head : req.body.newContentHead,
-      body : req.body.newContent
-  }
-
-  Post.create(newPost).then(function (){
-    res.redirect("/");
-  }).catch(function (err){
-    console.log(err);
-  });
-
+    Post.create(newPost).then(function (){
+      res.redirect("/");
+    }).catch(function (err){console.log(err)});
 });
 
 app.get("/posts/:id", function(req, res){
-  
   const postId = _.lowerCase(req.params.id);
 
   Post.find().then(function (posts){
     posts.forEach(function(post){
         if(_.lowerCase(post._id)===postId){
-            res.render("post", {PostHeading: post.head,
-                                PostContent: post.body});
+            res.render("post", {PostHeading: post.head, PostContent: post.body});
         }
     });
-  }).catch(function (err){
-    console.log(err);
-  });
-  
+  }).catch(function (err){console.log(err)});
 });
 
 app.listen(process.env.PORT_NO || 3000, function() {
